028b32b13de6ee9d864eb3fb47848c53e80a81b4
siddhanth-tiwari <pratimatiwari8299@gmail.com>
2024-06-11 21:46:44 +0530

    update exception

--- demo.py
+++ demo.py
@@ -6,6 +6,7 @@ logging.info("Welcome to custom log")
 from us_visa.exception import USvisaException
 import sys
 try:
-    a =1/0 "10"
+    a =1/"10"
 except Exception as e:
+    logging.info(e)
     raise USvisaException(e, sys) from e
