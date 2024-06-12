4cb0f36fa768bb71d83a5601a55e989cf07a4984
siddhanth-tiwari <pratimatiwari8299@gmail.com>
2024-06-12 17:59:08 +0530

    updated

--- demo.py
+++ demo.py
@@ -1,3 +1,7 @@
+# log file creating and exception file create 
+'''
+# 1 create log file
+
 from us_visa.logger import logging
 logging.info("Welcome to custom log")
 
@@ -10,3 +14,25 @@ try:
 except Exception as e:
     logging.info(e)
     raise USvisaException(e, sys) from e
+    
+'''
+# create artifact folder and data ingestion 
+# from us_visa.constants import DATABASE_NAME
+
+''' 
+# 3 TO GET COLLECTION NAME we can use way to get
+
+
+
+from us_visa.constants import*
+
+print (COLLECTION_NAME)
+'''
+
+# run artifact, train test  data set by mongodb
+
+from us_visa.pipline.training_pipeline import TrainPipeline
+
+
+pipline  = TrainPipeline()
+pipline.run_pipeline()
