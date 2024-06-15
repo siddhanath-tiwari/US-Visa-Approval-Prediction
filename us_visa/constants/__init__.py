126f19c29a20c72bc2091ebaa4b3ea763cf5b211
siddhanth-tiwari <pratimatiwari8299@gmail.com>
2024-06-15 12:27:18 +0530

    update

--- us_visa/constants/__init__.py
+++ us_visa/constants/__init__.py
@@ -25,7 +25,7 @@ SCHEMA_FILE_PATH = os.path.join("config", "schema.yaml")
 
 AWS_ACCESS_KEY_ID_ENV_KEY = "AWS_ACCESS_KEY_ID"
 AWS_SECRET_ACCESS_KEY_ENV_KEY = "AWS_SECRET_ACCESS_KEY"
-REGION_NAME = "us-east-1"
+REGION_NAME = "ap-south-1"
 
 
 
@@ -71,7 +71,7 @@ MODEL_TRAINER_MODEL_CONFIG_FILE_PATH: str = os.path.join("config", "model.yaml")
 
 
 MODEL_EVALUATION_CHANGED_THRESHOLD_SCORE: float = 0.02
-MODEL_BUCKET_NAME = "usvisa-model2024"
+MODEL_BUCKET_NAME = "usvisa-model2024-25"
 MODEL_PUSHER_S3_KEY = "model-registry"
 
 
